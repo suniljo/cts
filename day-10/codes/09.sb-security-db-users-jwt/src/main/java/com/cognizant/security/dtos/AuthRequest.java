38835e775ab8a3172895555d9005d0c13965d233
Sunil Joseph <sunilozhakal@gmail.com>
2026-06-04 11:31:55 +0530

    docker

--- day-10/codes/09.sb-security-db-users-jwt/src/main/java/com/cognizant/security/dtos/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.security.dtos;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String username;
-    private String password;
-}
--- day-11/java/codes/lambda-expressions/src/main/java/com/cognizant/java/lambdas/Nums.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.java.lambdas;
-
-@FunctionalInterface  //optional
-public interface Nums {
-	
-	public int add(int x, int y);
-	
-}
